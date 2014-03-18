da799e94deab791262616b72776c23b6e61f841a
Adam Bien <abien@adam-bien.com>
2014-03-18 20:36:40 +0100

    Example for ResourceBundle loading and use implemented

--- src/main/java/com/airhacks/followme/presentation/followme/FollowmePresenter.java
+++ src/main/java/com/airhacks/followme/presentation/followme/FollowmePresenter.java
@@ -1,7 +1,10 @@
 package com.airhacks.followme.presentation.followme;
 
 import com.airhacks.followme.business.flightcontrol.boundary.Tower;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javax.inject.Inject;
 
@@ -9,7 +12,7 @@ import javax.inject.Inject;
  *
  * @author adam-bien.com
  */
-public class FollowmePresenter {
+public class FollowmePresenter implements Initializable {
 
     @FXML
     Label message;
@@ -22,7 +25,16 @@ public class FollowmePresenter {
     @Inject
     private String happyEnding;
 
-    public void launch() {
-        message.setText(prefix + tower.readyToTakeoff() + happyEnding);
+    private String theVeryEnd;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //fetched from followme.properties
+        this.theVeryEnd = rb.getString("theEnd");
     }
+
+    public void launch() {
+        message.setText(prefix + tower.readyToTakeoff() + happyEnding + theVeryEnd);
+    }
+
 }
